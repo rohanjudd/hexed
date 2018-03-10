8d3e9295a37058b3656ce650ed2a58e4b0f2098d
Rohan <rohanjudd@gmail.com>
2018-03-10 10:57:27 +0000

    reverted hexed.ino, got bits to flip from button presses

--- hexed/button_test/button_test.ino
+++ hexed/button_test/button_test.ino
@@ -2,10 +2,18 @@
   Tests the function of all the inputs and outputs on the hexed game
 */
 
-#include "ssd1306.h"
-#include "font6x8.h"
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
 byte buttons[10] = {4, 5, 6, 7, 8, 9, 10, 12, 14, 15};
+byte button_byte = 0;
+byte last_button_byte = 0;
+byte new_presses = 0;
+byte guess = 0;
 byte pot = 17;
 byte led1 = 2;
 byte led2 = 3;
@@ -24,18 +32,24 @@ void setup()
   pinMode(led2, OUTPUT);
   pinMode(piezo, OUTPUT);
 
-  ssd1306_128x32_i2c_init();
-  ssd1306_fillScreen(0x00);
-  ssd1306_setFixedFont(ssd1306xled_font6x8);
-  ssd1306_printFixedN (0, 0, "HEXED", STYLE_BOLD, FONT_SIZE_2X);
-
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(100);
   Serial.begin(115200);
 }
 
 void loop()
 {
-  Serial.print("button byte: ");
-  Serial.println(get_button_byte());
+  button_byte = get_button_byte();
+  debug_byte("button_byte", button_byte);
+
+  update_new_presses();
+  debug_byte("new presses", new_presses);
+  
+  update_guess();
+  debug_byte("guess", guess);
+  
+  draw_byte(guess);
   Serial.print("button A: ");
   Serial.println(!digitalRead(buttons[8]));
   Serial.print("button B: ");
@@ -48,7 +62,7 @@ void loop()
   state = !state;
   freq = pot1 * 3 + (digitalRead(buttons[0]) * 200);
   //tone(piezo,freq);
-  delay(300);
+  delay(50);
 }
 
 byte get_button_byte()
@@ -59,5 +73,57 @@ byte get_button_byte()
     (!digitalRead(buttons[i])) ? (bitSet(output, i)) : (0);
   }
   return output;
+}
+
+void draw_byte(byte b)
+{
+  String s = get_binary_string(b);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.clearDisplay();
+  display.println(s);
+  display.display();
+}
+
+String get_binary_string(byte b)
+{
+  String output = "";
+  for (int i = 7; i >= 0; i--)
+  {
+    (bitRead(b, i)) ? (output += "1") : (output += "0");
+  }
+  return output;
+}
+
+void debug_byte(String s, byte b)
+{
+  Serial.print(s);
+  Serial.print(": ");
+  Serial.println(get_binary_string(b));
+}
+
+void update_guess()
+{
+  for(int i=0; i<=8; i++)
+  {
+    if(bitRead(new_presses, i))
+    {
+      bitWrite(guess, i, !bitRead(guess, i));
+    }
+  }
+}
+
+void update_new_presses()
+{
+  new_presses = 0;
+  for(int i=0; i<=8; i++)
+  {
+    if((bitRead(button_byte, i)) && (!bitRead(last_button_byte, i)))
+    {
+      bitSet(new_presses, i);
+    }
+  }
+  last_button_byte = button_byte;
 }
 
--- hexed/hexed.ino
+++ hexed/hexed.ino
@@ -1,34 +1,97 @@
- /* Read Quadrature Encoder
-   Connect Encoder to Pins encoder0PinA, encoder0PinB, and +5V.
+// Hexed Game
+// Rohan Judd Feb 2018
 
-   Sketch by max wolf / www.meso.net
-   v. 0.1 - very basic functions - mw 20061220
+#include "hex_byte.h"
+#include "game.h"
 
-*/
+const byte numChars = 9;
+char receivedChars[numChars]; // an array to store the received data
+boolean newData = false;
 
-int val;
-int encoder0PinA = 3;
-int encoder0PinB = 4;
-int encoder0Pos = 0;
-int encoder0PinALast = LOW;
-int n = LOW;
+Game hex_game(0);
 
-void setup() {
-  pinMode (encoder0PinA, INPUT_PULLUP);
-  pinMode (encoder0PinB, INPUT_PULLUP);
-  Serial.begin (9600);
+void setup()
+{
+  pinMode(A0, INPUT);
+  randomSeed(analogRead(A0));
+  Serial.begin(115200);
+  Serial.println("Hexed");
+  new_target();
 }
 
-void loop() {
-  n = digitalRead(encoder0PinA);
-  if ((encoder0PinALast == LOW) && (n == HIGH)) {
-    if (digitalRead(encoder0PinB) == LOW) {
-      encoder0Pos--;
-    } else {
-      encoder0Pos++;
+void loop()
+{
+  recvWithEndMarker();
+  if (newData == true)
+  {
+    Serial.println(receivedChars);
+    newData = false;
+    if (!check_for_mode_change())
+    {
+      hex_game.check_guess(receivedChars);
     }
-    Serial.print (encoder0Pos);
-    Serial.print ("/");
   }
-  encoder0PinALast = n;
 }
+
+boolean check_for_mode_change()
+{
+  if(receivedChars[0] == 'm')
+  {
+    if(isDigit(receivedChars[1]))
+    {
+      hex_game.change_mode(receivedChars[1] - 48);
+      return true;
+    }
+  }
+  return false;
+}
+
+void new_target()
+{
+  hex_game.new_target();
+}
+
+void print_all_formats(byte b)
+{
+  Serial.print("DEC: ");
+  Serial.print(b);
+  Serial.print("  HEX: ");
+  Serial.print(get_hex_string(b));
+  Serial.print("  BIN: ");
+  Serial.println(get_binary_string(b));
+}
+
+void print_hex(byte b)
+{
+  Serial.print("0x ");
+  Serial.println(get_hex_string(b));
+}
+
+void recvWithEndMarker()
+{
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+
+  while (Serial.available() > 0 && newData == false)
+  {
+    rc = Serial.read();
+    if (rc != endMarker)
+    {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars)
+      {
+        ndx = numChars - 1;
+      }
+    }
+    else
+    {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+
