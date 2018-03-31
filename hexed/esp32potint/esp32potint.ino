7475c8cc798c4ca0f97c3cb3736b602c83ef5a73
Rohan <rohanjudd@gmail.com>
2018-03-31 14:47:52 +0100

    pot int try

---
+++ hexed/esp32potint/esp32potint.ino
@@ -0,0 +1,58 @@
+// Stop button is attached to PIN 0 (IO0)
+#define BTN_STOP_ALARM    32
+
+hw_timer_t * timer = NULL;
+volatile SemaphoreHandle_t timerSemaphore;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+volatile uint32_t isrCounter = 0;
+volatile uint32_t lastIsrAt = 0;
+
+void IRAM_ATTR onTimer(){
+  // Increment the counter and set the time of ISR
+  portENTER_CRITICAL_ISR(&timerMux);
+  isrCounter++;
+  lastIsrAt = millis();
+  portEXIT_CRITICAL_ISR(&timerMux);
+  // Give a semaphore that we can check in the loop
+  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+  // It is safe to use digitalRead/Write here if you want to toggle an output
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting");
+  pinMode(BTN_STOP_ALARM, INPUT_PULLUP);
+  timerSemaphore = xSemaphoreCreateBinary();
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 10000, true);
+  timerAlarmEnable(timer);
+}
+
+void loop() {
+  // If Timer has fired
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE){
+    uint32_t isrCount = 0, isrTime = 0;
+    // Read the interrupt count and time
+    portENTER_CRITICAL(&timerMux);
+    isrCount = isrCounter;
+    isrTime = lastIsrAt;
+    portEXIT_CRITICAL(&timerMux);
+    // Print it
+    Serial.print("onTimer no. ");
+    Serial.print(isrCount);
+    Serial.print(" at ");
+    Serial.print(isrTime);
+    Serial.println(" ms");
+  }
+  // If button is pressed
+  if (digitalRead(BTN_STOP_ALARM) == LOW) {
+    // If timer is still running
+    if (timer) {
+      // Stop and free timer
+      timerEnd(timer);
+      timer = NULL;
+    }
+  }
+}
